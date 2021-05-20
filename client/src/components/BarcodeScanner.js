3045dd93f5fe465a573119cb89eedbc7e736ba3f
Mr. Llanes <llanesm@gmail.com>
2021-05-19 22:12:34 -0500

    Attempt with Barcode-Reader

--- client/src/components/BarcodeScanner.js
+++ client/src/components/BarcodeScanner.js
@@ -1,63 +1,23 @@
-import React, { useState, useEffect, useRef } from "react";
-import { Link } from "react-router-dom";
-import Quagga from "quagga";
-import { Fab, TextareaAutosize, Paper } from "@material-ui/core";
-import { ArrowBack } from "@material-ui/icons";
-import { quaggaConfig } from "../config/quagga";
-export const BarcodeScanner = () => {
-    const [results, setResults] = useState([]);
-    const [error, setError] = useState();
-    const [scanning, setScanning] = useState(false);
-    const barcodeRef = useRef(null);
+import React, { useState } from "react";
+import BarcodeReader from "react-barcode-reader";
 
-    const handleDetected = (result) => {
-        console.log(result);
-        setResults([...result]);
+export const BarcodeScanner = () => {
+    const [result, setResult] = useState([]);
+    const [error, setError] = useState(false);
+    const handleScan = (data) => {
+        setResult(data);
     };
-    const startQuagga = () => {
-        Quagga.init(quaggaConfig, (err) => {
-            if (err) {
-                setError(err);
-                return console.log(err);
-            }
-            console.log("WORRRKKKK");
-            setScanning(true);
-            Quagga.start();
-        });
-        Quagga.onDetected(handleDetected);
-        Quagga.onProcessed((data) => console.log(data));
-        Quagga.offDetected(handleDetected);
+    const handleError = (err) => {
+        console.error(err);
+        setError(err);
     };
     return (
-        <div>
-            <Link to="/">
-                <Fab style={{ marginRight: 10 }} color="secondary">
-                    <ArrowBack />
-                </Fab>
-            </Link>
-            <span>Barcode Scanner</span>
-            <button type="button" onClick={startQuagga}>
-                Scan Barcode
-            </button>
-            <Paper
-                variant="outlined"
-                style={{ marginTop: 30, width: 640, height: 320 }}
-            >
-                <div id="interactive" className="viewport" />
-            </Paper>
-            <TextareaAutosize
-                style={{
-                    fontSize: 32,
-                    width: 320,
-                    height: 100,
-                    marginTop: 30,
-                }}
-                rowsMax={4}
-                defaultValue={"No data scanned"}
-                value={
-                    results[0] ? results[0].codeResult.code : "No data scanned"
-                }
-            />
-        </div>
+        <>
+            {error}
+            <div>
+                <BarcodeReader onError={handleError} onScan={handleScan} />
+                <p>{result}</p>
+            </div>
+        </>
     );
 };
