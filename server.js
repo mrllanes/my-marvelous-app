692d1c95a5552fa70a9b1838811b435a4acf74f9
Mr. Llanes <llanesm@gmail.com>
2021-05-19 21:32:27 -0500

    Changes to server.js again.

--- server.js
+++ server.js
@@ -25,15 +25,12 @@ mongoose.connect(
     }
 );
 
-// mongoose.Promise = global.Promise;
-// const db = mongoose.connection;
-
 app.use("/api", routes);
 
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    res.sendFile(path.resolve(".", "./client/build/index.html"));
 });
 
 app.listen(PORT, () => {
