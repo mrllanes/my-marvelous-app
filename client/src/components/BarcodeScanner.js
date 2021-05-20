e52ec22596101345bbe69f43161cecba07fe6139
Mr. Llanes <llanesm@gmail.com>
2021-05-19 23:03:39 -0500

    Attempt with react-webcam-barcode-scanner, modifications.

--- client/src/components/BarcodeScanner.js
+++ client/src/components/BarcodeScanner.js
@@ -1,8 +1,34 @@
 import React, { useState } from "react";
 import BarcodeScannerComponent from "react-webcam-barcode-scanner";
+import { ComicCard } from "./ComicCard";
+import API from "../utils/API";
 
 export const BarcodeScanner = () => {
+    const [comics, setComics] = useState([]);
     const [data, setData] = React.useState("Not Found");
+    const searchComics = (text) => {
+        API.getComics(text)
+            .then((results) => {
+                console.log(results.data.data.results);
+                setComics(results.data.data.results);
+            })
+            .catch((error) => {
+                console.log(error);
+                setComics([]);
+            });
+    };
+
+    const saveComic = (data) => {
+        API.saveComic({
+            title: data.title,
+            series: data.series.name,
+            creators: data.creators.items,
+            description: data.description,
+            image: data.thumbnail.path,
+        })
+            .then((res) => console.log(res))
+            .catch((err) => console.log(err));
+    };
 
     return (
         <>
@@ -10,11 +36,31 @@ export const BarcodeScanner = () => {
                 width={500}
                 height={500}
                 onUpdate={(err, result) => {
-                    if (result) setData(result.text);
-                    else setData("Not Found");
+                    if (result) {
+                        setData(result.text);
+                        searchComics(result.text);
+                    } else setData("Not Found");
                 }}
             />
             <p>{data}</p>{" "}
+            <div className="container text-center">
+                <div className="row">
+                    <div className="col">
+                        {comics.map((data) => (
+                            <div key={data.id} classname="col">
+                                <ComicCard
+                                    title={data.title}
+                                    series={data.series.name}
+                                    creators={data.creators.items}
+                                    description={data.description}
+                                    image={data.thumbnail.path}
+                                    comicSave={() => saveComic(data)}
+                                />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
